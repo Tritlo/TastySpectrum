ee5601bf6ac3a532e51c16858edd39b5d83496a0
Matthías Páll Gissurarson <mpg@mpg.is>
2023-10-09 15:02:58 +0200

    Add tree rendering of CSV results, remove space in CSV output

---
+++ app/Main.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE RecordWildCards #-}
+module Main where
+
+
+
+import Options.Applicative
+import Data.Tree (drawForest)
+
+import Test.Tasty.Ingredients.Spectrum.CSVToForest
+
+-- | convert Mix files and CSV to a tree.
+data Config = Conf {
+        target_file :: FilePath
+    } deriving (Eq, Show)
+
+
+config :: Parser Config
+config = Conf <$> argument str (metavar "TARGET" <> help "CSV file to convert to a tree")
+              -- <*> strOption (long "hpc-dir" <> metavar "HPC_DIR" <> help "Location of the mix files" <> showDefault <> value ".hpc/")
+
+
+opts :: ParserInfo Config
+opts = info (config <**> helper)
+        (fullDesc <>
+         progDesc "Convert a tasty spectrum CSV file TARGET to a tree" <>
+         header "mix-csv-to-tree")
+
+main :: IO ()
+main = do Conf {..} <- execParser opts
+          (test_results, forest) <- csvToForest target_file
+          putStrLn $ drawForest $ map (fmap show) forest
+
