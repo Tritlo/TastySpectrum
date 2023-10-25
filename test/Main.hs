e8aeb24f8fb2a5058b47a399d32442aca5da2739
Matthías Páll Gissurarson <mpg@mpg.is>
2023-10-25 15:44:45 +0200

    Test tarantula and dstar

--- test/Main.hs
+++ test/Main.hs
@@ -7,6 +7,8 @@ import Test.Tasty.HUnit
 import Test.Tasty.Ingredients.Spectrum
 import Data.List
 
+import Test.Tasty.Ingredients.Spectrum.ParseCSV
+import Test.Tasty.Ingredients.Spectrum.SBFL
 
 main :: IO ()
 main = defaultMainWithIngredients (testSpectrum:defaultIngredients) tests
@@ -38,4 +40,17 @@ unitTests = testGroup "Unit tests"
   -- the following test does not hold
   , testCase "List comparison (same length)" $
       [1 :: Integer, 2, 3] `compare` [1,2,2] @?= LT
+  , testCase "Tarantula works" $
+     (tarantula <$> parseCSV "test/tarantula.csv") >>=
+       checkExpected [0.5,0.5,0.5,0.63,0.0,0.71,0.83,0.0,0.0,0.0,0.0,0.5] . map snd
+  , testCase "Dstar 2 works" $
+     (dstar 2 <$> parseCSV "test/dstar.csv") >>=
+       checkExpected [2.0,2.0,2.29,2.29,0.13,3.0,0.25,1.33,0.0] . map snd
+  , testCase "Dstar 3 works" $
+     (dstar 3 <$> parseCSV "test/dstar.csv") >>=
+       checkExpected [8.0,8.0,9.14,9.14,0.13,9.0,0.25,2.67,0.0] . map snd
   ]
+  where checkExpected expected res = 
+            assertBool ("Result and expected don't match, expected: "
+                        <> show expected <> ", got: " <> show res) $
+                    all (\(e,r) -> abs (e-r) < 0.1) $ zip expected res
