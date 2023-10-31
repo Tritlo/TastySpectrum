bfa6e2d1efc7a007669f6fb874cf09b74db7cc10
Matthías Páll Gissurarson <mpg@mpg.is>
2023-10-31 21:05:52 +0100

    Show leaf and root distance, ignore prefix

--- app/Main.hs
+++ app/Main.hs
@@ -10,8 +10,8 @@ import Test.Tasty.Ingredients.Spectrum.ParseCSV
 import Test.Tasty.Ingredients.Spectrum.Types
 import Test.Tasty.Ingredients.Spectrum.SBFL
 
-import Data.List (sortOn)
-import qualified Data.Set as Set
+import Data.List (sortOn, isPrefixOf)
+import qualified Data.Map as Map
 
 
 -- | convert Mix files and CSV to a tree.
@@ -19,7 +19,9 @@ data Config = Conf {
         target_file :: !FilePath,
         opt_command :: !Command,
         opt_limit :: !Int,
-        ignore :: !String
+        ignore :: !String,
+        show_leaf_distance :: !Bool,
+        show_root_distance :: !Bool
 
     } deriving (Eq, Show)
 
@@ -36,6 +38,8 @@ config = Conf <$> argument str (metavar "TARGET" <> help "CSV file to use")
              <*> option auto (long "limit" <> value 0
                             <> short 'n' <> metavar "LIMIT" <> help "The number of results to show")
              <*> (strOption (long "ignore" <> value "" <> metavar "IGNORE" <> help "Paths to ignore (e.g. 'tests/Main.hs src/Data/Module/File.hs')"))
+             <*> switch (long "show-leaf-distance" <> help "Display the distance of the expression from a leaf node")
+             <*> switch (long "show-root-distance" <> help "Display the distance of the expression from a root node")
     where 
           treeCommand :: Mod CommandFields Command
           treeCommand = command "tree" (info (pure Tree) (progDesc "Show a tree of the results"))
@@ -47,7 +51,6 @@ config = Conf <$> argument str (metavar "TARGET" <> help "CSV file to use")
           dstarCommand = command "dstar" (info dstarOpts (progDesc "Use the dstar algorithm with the given k"))
           dstarOpts :: Parser Command
           dstarOpts = DStar <$> argument auto (metavar "K" <> help "The k to use for dstar")
-              -- <*> strOption (long "hpc-dir" <> metavar "HPC_DIR" <> help "Location of the mix files" <> showDefault <> value ".hpc/")
 
 
 opts :: ParserInfo Config
@@ -63,8 +66,9 @@ main = do Conf {..} <- execParser opts
           tr'@(test_results, labeled') <- parseCSV target_file
           let limit = if opt_limit <= 0 then id else take opt_limit
               labeled  = if null ignore then labeled'
-                         else let paths = Set.fromList (words ignore)
-                              in filter (not . flip Set.member paths . loc_name) labeled'
+                         else let prefixes = map isPrefixOf $ words ignore
+                                  anyPrefix loc = any ($ loc) prefixes
+                              in filter (not . anyPrefix . loc_name) labeled'
               tr = (test_results,labeled)
           case opt_command of 
             Tree -> putStrLn $ drawForest $ map (fmap show) 
@@ -75,7 +79,15 @@ main = do Conf {..} <- execParser opts
                               DStar k -> dstar k
                        res = sortOn ((\i -> -i) . snd) $ sf tr
                        ppr ((Label {loc_name=ln, loc_pos=lp}), score) =
-                        ln ++ ":" ++ (show lp) ++ " " ++ show score
-                   in mapM_ (putStrLn . ppr) $ limit res
+                        ln <> ":" <> show lp <> " " <> show score
+                       
+                   in if (show_leaf_distance || show_root_distance)
+                      then let ds =  if show_leaf_distance
+                                     then leafDistances labeled
+                                     else rootDistances labeled
+                               ppr' b@(l,_) =
+                                    ppr b <> " " <> show (ds Map.! l)
+                            in mapM_ (putStrLn . ppr') $ limit res
+                      else mapM_ (putStrLn . ppr) $ limit res
 
 
--- app/Test/Tasty/Ingredients/Spectrum/GenForest.hs
+++ app/Test/Tasty/Ingredients/Spectrum/GenForest.hs
@@ -1,4 +1,5 @@
-module Test.Tasty.Ingredients.Spectrum.GenForest (genForest) where
+module Test.Tasty.Ingredients.Spectrum.GenForest (genForest, leafDistances,
+                                                             rootDistances) where
 
 
 import Test.Tasty.Ingredients.Spectrum.Types
@@ -14,7 +15,37 @@ import qualified Data.List as L
 import qualified Data.Map.Strict as Map
 import Data.Map (Map)
 import Data.Function (on)
-    
+   
+
+
+-- Not very efficient?
+leafDistances :: [Label] -> Map Label Int
+leafDistances labels =
+    Map.fromList $ map (\l -> (l, Set.size $ contains nodeSet l)) labels
+    where nodeSet = genNodeSet labels
+
+rootDistances :: [Label] -> Map Label Int
+rootDistances labels =
+    Map.fromList $ map (\l -> (l, Set.size $ containedBy nodeSet l)) labels
+    where nodeSet = genNodeSet labels
+          
+
+genNodeSet :: [Label] -> Map String (Set Label)
+genNodeSet all_nodes = Map.fromAscList $
+    map (\g@(h:_) -> (loc_name h, Set.fromList g)) $
+       L.groupBy ((==) `on` loc_name) $
+       L.sortBy (compare `on` loc_name) $  all_nodes
+
+-- How many locations contain this location
+containedBy :: Map String (Set Label) -> Label -> Set Label
+containedBy nodeSet n = ns''
+    where ns' = Set.delete n (nodeSet Map.! (loc_name n))
+          ns'' = Set.filter (insideHpcPos (loc_pos n) . loc_pos) ns'
+
+contains :: Map String (Set Label) -> Label -> Set Label
+contains nodeSet n = ns''
+    where ns' = Set.delete n (nodeSet Map.! (loc_name n))
+          ns'' = Set.filter (flip insideHpcPos (loc_pos n) . loc_pos) ns'
 
 -- | This function orders a List of Labels (from a spectrum.csv) into multiple trees. 
 --   This is done by looking if one label contains another (based on source-code spans) 
@@ -22,15 +53,10 @@ import Data.Function (on)
 genForest :: [Label] -> Forest Label
 genForest all_nodes = map toTreeF roots_and_children
   where nodeSet :: Map String (Set Label)
-        nodeSet = Map.fromAscList $
-                       map (\g@(h:_) -> (loc_name h, Set.fromList g)) $
-                       L.groupBy ((==) `on` loc_name) $
-                       L.sortBy (compare `on` loc_name) $  all_nodes
-        containedBy n = (n,ns'')
-            where ns' = Set.delete n (nodeSet Map.! (loc_name n))
-                  ns'' = Set.filter (insideHpcPos (loc_pos n) . loc_pos) ns'
+        nodeSet = genNodeSet all_nodes
+        containedBy' = containedBy nodeSet
         contained :: [(Label, Set Label)]
-        contained = map containedBy all_nodes
+        contained = map (\n -> (n, containedBy' n))  all_nodes
         roots :: [Label]
         roots = map fst $ filter (Set.null . snd) contained
         roots_and_children = 
