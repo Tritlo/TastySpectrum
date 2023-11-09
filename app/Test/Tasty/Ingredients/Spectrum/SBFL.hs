df70c7d0f953cffb6faadcce47ea7ee7cb57f53a
Matthías Páll Gissurarson <mpg@mpg.is>
2023-11-09 11:48:34 +0100

    Little fixes

--- app/Test/Tasty/Ingredients/Spectrum/SBFL.hs
+++ app/Test/Tasty/Ingredients/Spectrum/SBFL.hs
@@ -3,7 +3,7 @@ module Test.Tasty.Ingredients.Spectrum.SBFL where
 import Test.Tasty.Ingredients.Spectrum.Types
 
 -- ! DevNote: The Formulas have been extracted from the following Publication:
--- Should I follow this fault localization tool’s output? https://link.springer.com/article/10.1007/s10664-014-9349-1 
+-- Should I follow this fault localization tool’s output? https://link.springer.com/article/10.1007/s10664-014-9349-1
 -- It is a comparative study that lists all formulas in a similar format
 
 
@@ -18,14 +18,14 @@ totalPassFail (res,_) = (toInteger p, toInteger f)
         f = length res - p
 
 -- | Returns the number of passing and failing tests for a single spectrum-expression
--- TODO: should we be extra save and filter out 0`s ? 
-passFail :: 
-      Label                -- ^ A source code location, including the 
+-- TODO: should we be extra save and filter out 0`s ?
+passFail ::
+      Label                -- ^ A source code location, including the
       -> (Integer,Integer) -- ^ (NumberOfPassing,NumberOfFailing)-Tests
 passFail (Label {loc_evals=evals}) = (toInteger p, toInteger f)
     where p = length $ filter (>0) $ map snd evals
           f = length evals - p
-    
+
 -- | The Tarantula Formula
 -- Relevant Publication: https://dl.acm.org/doi/abs/10.1145/1101908.1101949
 tarantula :: TestResults -> [(Label, Double)]
@@ -37,16 +37,18 @@ tarantula r@(test_results, labeled) = map (\l -> (l, ttula l)) labeled
                   ptp = (fromInteger p)/(fromInteger tp)
 
 -- | OCHIAI Formula
--- Original Paper is from Biology and Genetics, so best SE Paper is likely https://link.springer.com/article/10.1007/s10664-014-9349-1
+-- Original Paper is from Biology and Genetics, so best SE Paper is likely
+-- https://link.springer.com/article/10.1007/s10664-014-9349-1
 ochiai :: TestResults -> [(Label, Double)]
 ochiai r@(test_results, labeled) = map (\l -> (l, oc l)) labeled
     where (_,tf) = totalPassFail r
           oc label = (fromInteger f)/(sqrt $fromInteger $ tf*(p+f))
             where (p,f) = passFail label
+
 -- | The DStar Formula
 -- Relevant Publication: https://doi.org/10.1109/TR.2013.2285319
 dstar :: Integer -> TestResults -> [(Label, Double)]
-dstar k r@(test_results, labeled) 
+dstar k r@(test_results, labeled)
       | k <= 0 = error "DStar requires k>=1"
       | otherwise = map (\l -> (l, ds l)) labeled
     where (_,tf) = totalPassFail r
