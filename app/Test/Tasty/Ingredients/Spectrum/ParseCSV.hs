45f2578adfd18bd5775dcfecee624021c74e7032
Matthías Páll Gissurarson <mpg@mpg.is>
2023-11-13 22:04:05 +0100

    Manually parse again, more efficient

--- app/Test/Tasty/Ingredients/Spectrum/ParseCSV.hs
+++ app/Test/Tasty/Ingredients/Spectrum/ParseCSV.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE PatternGuards #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 module Test.Tasty.Ingredients.Spectrum.ParseCSV where
 
 import Test.Tasty.Ingredients.Spectrum.Types
@@ -15,59 +16,71 @@ import qualified Data.Map.Strict as Map
 import qualified Data.IntMap.Strict as IM
 import Data.IntMap (IntMap)
 
-import Data.Attoparsec.Text
-import qualified Data.Attoparsec.Text as AT
 
-                            
-attoParse :: Parser ([(String, (Int,Int,Int,Int))], -- Locs
-                     [((String,String),
-                       Bool, [Integer])]) 
-attoParse = do locs <- parseHeader
-               endOfLine
-               entries <- sepBy1 (parseEntry (length locs)) endOfLine
-               -- endOfInput
-               return (locs, entries)
-  where parseHeader = string "test_name,test_type,test_result,"
-                      >> sepBy1 parseLoc (char ',')
-          where parseLoc :: Parser (String ,(Int,Int,Int,Int))
-                parseLoc = do char '"'
-                              fn <- manyTill anyChar (char ':')
-                              l1 <- manyTill anyChar (char ':')
-                              c1 <- manyTill anyChar (char '-')
-                              l2 <- manyTill anyChar (char ':')
-                              c2 <- manyTill anyChar (char '"')
-                              return (fn, (read @Int l1, read @Int c1,
-                                           read @Int l2, read @Int c2))
-        parseEntry :: Int -> Parser ((String,String), Bool, [Integer])
-        parseEntry num = do t_name <- parseString
-                            char ','
-                            t_type <- parseString
-                            char ','
-                            t_res <- parseBool 
-                            evals <- count num (char ',' >> signed decimal)
-                            return ((T.unpack t_name, T.unpack t_type), t_res, evals)
-        
-        parseBool :: Parser Bool
-        parseBool = choice [string "True" >> return True,
-                            string "False" >> return False]
-        parseString :: Parser T.Text
-        -- We just take until we get an unescaped '"'
-        parseString = do char '"'
-                         (t, _) <- match $ 
-                                    manyTill anyChar (satisfy (/= '\\') >> char '"')
-                         case T.unsnoc t of -- O(1)
-                            Just (t', '"') -> return t'
-                            _ -> fail "Unmatched string!"
-                                     
+
+parseHeader :: T.Text -> [(String, (Int,Int,Int,Int))]
+parseHeader h = case T.stripPrefix ("test_name,test_type,test_result,") h of
+                  Just r -> map parseLoc $ T.splitOn "," r
+                  _ -> error "Invalid header"
+  where parseLoc :: T.Text -> (String, (Int,Int,Int,Int))
+        parseLoc t = case p of
+              Just (!fn,t@(!a,!b,!c,!d)) -> (T.unpack fn, t)
+              _ -> error $ "invalid loc " <> (T.unpack t)
+         where p :: Maybe (T.Text, (Int,Int,Int,Int))
+               p = do ('"',!r) <- T.uncons t
+                      (!fn, !r) <- return (T.break (== ':') r)
+                      (':',!r) <- T.uncons r
+                      (!l1, !r) <- return (T.break (== ':') r)
+                      (':',!r) <- T.uncons r
+                      (!c1, !r) <- return (T.break (== '-') r)
+                      ('-',!r) <- T.uncons r
+                      (!l2, !r) <- return (T.break (== ':') r)
+                      (':',!r) <- T.uncons r
+                      (!c2,'"') <- T.unsnoc r
+                      return (fn, (read @Int $ T.unpack l1,
+                                   read @Int $ T.unpack c1,
+                                   read @Int $ T.unpack l2,
+                                   read @Int $ T.unpack c2))
+   
+parseEntry :: T.Text -> ((String,String), Bool, [Integer])
+parseEntry t = case p of 
+                Just ((tn,tt),tr,evs) -> ((T.unpack tn, T.unpack tt),tr,evs)
+                _ -> error $ "invalid entry " <> T.unpack t
+  where p = do (t_name,r) <- parseString t
+               (',', r) <- T.uncons r
+               (t_type,r) <- parseString r
+               (',', r) <- T.uncons r
+               (t_res, r) <- parseBool r
+               (',', r) <- T.uncons r
+               nums <- return $ map (read @Integer . T.unpack) $ T.splitOn "," r
+               return ((t_name,t_type), t_res, nums)
+
+        parseBool  :: T.Text -> Maybe (Bool, T.Text)
+        parseBool t = if T.isPrefixOf "True" t
+                      then (True,) <$> T.stripPrefix "True" t
+                      else if T.isPrefixOf "False" t
+                           then (False,) <$> T.stripPrefix "False" t
+                           else error ("invalid bool " <> T.unpack t)
+        parseString :: T.Text -> Maybe (T.Text, T.Text)
+        parseString s = do ('"',r) <- T.uncons s
+                           (str,res) <- go r
+                           return (str, T.drop 1 res)
+             where go r = let (pf, rs) = T.break (== '"') r
+                          in if (T.take 1 rs) /= "\""
+                             then error $ "unmatched string " <> (T.unpack s)
+                             else case T.unsnoc pf of
+                                    Just (pfc,c) | c == '\\' ->
+                                        do (r2, res2) <- go $ T.drop 1 rs
+                                           return (pf <> "\"" <> r2, res2)
+                                    _ -> return (pf,rs)
+                    
 
 parseCSV :: FilePath -> IO ([((String,String), Bool)], IM.IntMap String, [Label])
 parseCSV target_file = do
-          f <- TIO.readFile target_file
-          let p_res = parseOnly attoParse f
-          (locs, parsed) <- case p_res of
-                             Left err -> error ("Got error while parsing :" <>  err)
-                             Right r -> return r
-          let groups = map head $ group $ sort $ map (\(s,_) -> s) $ locs
+          (h:rs) <- T.lines <$> TIO.readFile target_file
+          let !locs = parseHeader h 
+              !parsed = map parseEntry rs
+              groups = map head $ group $ sort $ map (\(s,_) -> s) $ locs
 
               findGroup = Map.fromAscList $ zip groups [0..]
 
